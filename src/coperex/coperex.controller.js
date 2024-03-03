884a27846453cbf1590f27e55a8433eb5488d698
Angelo_Palma <apalma-2022125@kinal.edu.gt>
2024-03-03 01:11:07 -0600

    Se finalizo el crud de coperex

---
+++ src/coperex/coperex.controller.js
@@ -0,0 +1,60 @@
+import Coperex from './coperex.model.js';
+
+
+export const coperexGet = async (req, res) =>{
+    const {limite, desde } = req.query;
+    const query = {estado: true};
+
+    const [total, coperexs] = await Promise.all([
+        Coperex.countDocuments(query),
+        Coperex.find(query)
+        .skip(Number(desde))
+        .limit(Number(limite))
+    ]);
+
+    res.status(200).json({
+        total,
+        coperexs
+    });
+}
+
+export const getCoperexById = async (req, res) =>{
+    const {id} = req.params;
+    const coperex = await Coperex.findOne({_id: id});
+
+    res.status(200).json({
+        coperex
+    });
+}
+
+
+
+
+export const coperexPut = async (req, res) =>{
+    const {id} = req.params;
+    const {_id, ...resto} = req.body;
+
+    await Coperex.findByIdAndUpdate(id, resto);
+
+    const coperex = await Coperex.findOne({_id: id});
+
+    res.status(200).json({
+        msg: 'Company updated',
+        coperex
+    });
+}
+
+
+
+
+export const coperexPost = async (req, res) => {
+    const {name, levelOfImpact, yearsOfExperience, category } = req.body;
+    const coperex = new Coperex ({name , levelOfImpact, yearsOfExperience: parseInt(yearsOfExperience), category});
+
+    await empresa.save();
+
+    res.status(200).json({
+        coperex
+    });
+
+}
